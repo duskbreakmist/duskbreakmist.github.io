2a7689339014c1f58a8812a6d4896bcb2dda22f2
dusk <you@example.com>
2022-05-12 23:48:50 +0800

    add html try

---
+++ js/index_js.js
@@ -0,0 +1,10 @@
+function changetitle(){
+    document.getElementById("title").innerHTML = "has changed";
+}
+
+function writelog(a){
+    console.log(a);
+}
+function hid(){
+    document.getElementById("hid").innerHTML = "";
+}
